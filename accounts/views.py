007421a53be0c221fd1a45e4336783c7c2123e12
shashankmistry31 <shashankmistry31@gmail.com>
2020-09-12 12:50:56 +0530

    Optimization-fifth commit

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,8 @@ from .forms import *
 from django.contrib import messages
 from django.contrib.sessions.backends.db import SessionStore
 from datetime import datetime
-import requests
+import requests ,time
+db_time = time.time()
 
 def home(request):
     converted = ''
@@ -22,13 +23,11 @@ def home(request):
             db_timestamp = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
             db_input = form.cleaned_data['query']
             db_type = 2
-            url = "https://jkl86ei2j1.execute-api.ap-southeast-1.amazonaws.com/quality/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp) +"&db_input="+db_input+"&db_type=2"
-            response = requests.get(url)
-            #print(db_session_key,db_timestamp,db_input,db_type)
-            bin = form.cleaned_data['query']
-
-            if bin :
-                query_resp = requests.get('https://zscga0herg.execute-api.ap-southeast-1.amazonaws.com/default/lexdemo?operand1='+bin)
+            if db_input :
+                url = "https://jkl86ei2j1.execute-api.ap-southeast-1.amazonaws.com/quality/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp)+'||'+str(db_time) +"&db_input="+db_input+"&db_type=2"
+                print(url)
+                query_resp = requests.get(url)
+                #query_resp = requests.get('https://zscga0herg.execute-api.ap-southeast-1.amazonaws.com/default/lexdemo?operand1='+bin)
                 try:
                     query_resp.raise_for_status()
                     converted = query_resp.json()['response']['message']
@@ -38,14 +37,6 @@ def home(request):
                     context= {'form':form ,'converted':converted,'alert_flag': True}
                     return render(request,'accounts/dashboard.html',context)
 
-            # if bin.isdigit():
-            #     try:
-            #         converted  = int(bin,2)
-            #         print(converted)
-            #     except:
-            #         form = BinForm()
-            #         context= {'form':form ,'converted':converted,'alert_flag': True}
-            #         return render(request,'accounts/dashboard.html',context)
             else:
                 context= {'form':form ,'converted':converted,'alert_flag': True}
                 return render(request,'accounts/dashboard.html',context)
@@ -57,7 +48,7 @@ def home(request):
     else:
         db_session_key= request.session.session_key
         db_timestamp = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
-        url = "https://jkl86ei2j1.execute-api.ap-southeast-1.amazonaws.com/quality/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp) +"&db_input=NULL"+"&db_type=1"
+        url = "https://jkl86ei2j1.execute-api.ap-southeast-1.amazonaws.com/quality/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp)+'||'+str(db_time) +"&db_input=NULL"+"&db_type=1"
         response = requests.get(url)
         form = BinForm()
     context= {'form':form ,'converted':converted}
@@ -75,13 +66,11 @@ def d2b(request):
             db_session_key= request.session.session_key
             db_timestamp = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
             db_input = form.cleaned_data['decimal_data']
-            url = "https://jkl86ei2j1.execute-api.ap-southeast-1.amazonaws.com/quality/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp) +"&db_input="+db_input+"&db_type=4"
+            url = "https://jkl86ei2j1.execute-api.ap-southeast-1.amazonaws.com/quality/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp)+'||'+str(db_time) +"&db_input="+db_input+"&db_type=4"
             response = requests.get(url)
-            dec = form.cleaned_data['decimal_data']
-            print(dec)
-            if dec.isdigit():
+            if db_input.isdigit():
                 try:
-                    converted  = bin(int(dec))[2:]
+                    converted  = bin(int(db_input))[2:]
                     print(converted)
                 except:
                      form = DecForm()
@@ -98,7 +87,7 @@ def d2b(request):
     else:
         db_session_key= request.session.session_key
         db_timestamp = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
-        url = "https://jkl86ei2j1.execute-api.ap-southeast-1.amazonaws.com/quality/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp) +"&db_input=NULL"+"&db_type=3"
+        url = "https://jkl86ei2j1.execute-api.ap-southeast-1.amazonaws.com/quality/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp)+'||'+str(db_time) +"&db_input=NULL"+"&db_type=3"
         response = requests.get(url)
         form = DecForm()
     context= {'form':form ,'converted':converted}
