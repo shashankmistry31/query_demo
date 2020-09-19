37acdd5224e0f0ef9a9e9ca3b8cb4a4cdb16e4fd
shashankmistry31 <shashankmistry31@gmail.com>
2020-09-19 08:39:09 +0530

    added Intent list functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home,name='home'),
     # path('home/', views.home),
     path('d2b/', views.d2b,name='d2b'),
+    path('askintent/<str:pk>',views.askintent ,name='askintent')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,8 +5,8 @@ from django.http import  HttpResponseRedirect
 from .forms import *
 from django.contrib import messages
 from django.contrib.sessions.backends.db import SessionStore
-from datetime import datetime
-import requests ,time
+#from datetime import datetime
+import requests #,time
 
 def home(request):
     converted = ''
@@ -19,19 +19,22 @@ def home(request):
         form = BinForm(request.POST)
         if form.is_valid():
             db_session_key= request.session.session_key
-            db_timestamp = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
+            #db_timestamp = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
             db_input = form.cleaned_data['query']
-            db_time = time.time()
+            #db_time = time.time()
             db_type = 2
             if db_input :
-                url = "https://xui0vb3wa8.execute-api.us-east-1.amazonaws.com/prod/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp)+'||'+str(db_time) +"&db_input="+db_input+"&db_type=2"
+                #Christian AWS account url
+                url = "https://t41v93n5u5.execute-api.eu-west-2.amazonaws.com/prod/processask?session_key="+db_session_key+"&db_input="+db_input+"&db_type=2"
+                #Shashank AWS account url
+                #url = "https://7q539nw8rl.execute-api.ap-southeast-1.amazonaws.com/default/dynamodb_update?session_key="+db_session_key+"&db_input="+db_input+"&db_type=2"
                 print(url)
                 query_resp = requests.get(url)
-                #query_resp = requests.get('https://zscga0herg.execute-api.ap-southeast-1.amazonaws.com/default/lexdemo?operand1='+bin)
                 try:
                     query_resp.raise_for_status()
-                    converted = query_resp.json()['response']['message']
-                    context= {'form':form ,'converted':converted,'alert_flag': True}
+                    converted = query_resp.json()['Lex Response']['message']
+                    intents = query_resp.json()['Intent List']
+                    context= {'form':form ,'converted':converted,'alert_flag': True,'intents':intents}
                 except:
                     converted = 'Sorry .Something went wrong.'
                     context= {'form':form ,'converted':converted,'alert_flag': True}
@@ -47,12 +50,18 @@ def home(request):
             return HttpResponse('/Nothanks/')
     else:
         db_session_key= request.session.session_key
-        db_time = time.time()
-        db_timestamp = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
-        url = "https://xui0vb3wa8.execute-api.us-east-1.amazonaws.com/prod/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp)+'||'+str(db_time) +"&db_input=NULL"+"&db_type=1"
+        #db_time = time.time()
+        #db_timestamp = datetime.now().strftime("%Y-%m-%d,%H:%M:%S")
+        url = "https://t41v93n5u5.execute-api.eu-west-2.amazonaws.com/prod/processask?session_key="+db_session_key+"&db_input=NULL&db_type=1"
+        #mistry url
+        #url = "https://7q539nw8rl.execute-api.ap-southeast-1.amazonaws.com/default/dynamodb_update?session_key="+db_session_key+"&db_input=NULL&db_type=1"
+        print(url)
         response = requests.get(url)
+        print(response)
+        intents = response.json()['Intent List']
+        print(intents)
         form = BinForm()
-    context= {'form':form ,'converted':converted}
+    context= {'form':form ,'converted':converted,'intents':intents}
     return render(request,'accounts/dashboard.html',context)
 
 
@@ -65,10 +74,13 @@ def d2b(request):
         form = DecForm(request.POST)
         if form.is_valid():
             db_session_key= request.session.session_key
-            db_timestamp = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
-            db_time = time.time()
+            #db_timestamp = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
+            #db_time = time.time()
             db_input = form.cleaned_data['decimal_data']
-            url = "https://xui0vb3wa8.execute-api.us-east-1.amazonaws.com/prod/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp)+'||'+str(db_time) +"&db_input="+db_input+"&db_type=4"
+            #Christian url
+            url = "https://t41v93n5u5.execute-api.eu-west-2.amazonaws.com/prod/processask?session_key="+db_session_key+"&db_input="+db_input+"&db_type=4"
+            #Shashank Url
+            #url = "https://7q539nw8rl.execute-api.ap-southeast-1.amazonaws.com/default/dynamodb_update?session_key="+db_session_key+"&db_input="+db_input+"&db_type=4"
             response = requests.get(url)
             if db_input.isdigit():
                 try:
@@ -88,10 +100,25 @@ def d2b(request):
             return HttpResponse('/Nothanks/')
     else:
         db_session_key= request.session.session_key
-        db_timestamp = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
-        db_time = time.time()
-        url = "https://xui0vb3wa8.execute-api.us-east-1.amazonaws.com/prod/dynamodb-update?session_key="+db_session_key+"&timestamp="+ str(db_timestamp)+'||'+str(db_time) +"&db_input=NULL"+"&db_type=3"
+        #db_timestamp = datetime.now().strftime("%Y-%m-%d|%H:%M:%S")
+        #db_time = time.time()
+        #Christian url
+        url = "https://t41v93n5u5.execute-api.eu-west-2.amazonaws.com/prod/processask?session_key="+db_session_key+"&db_input=NULL&db_type=3"
+        #Shashank url
+        #url = "https://7q539nw8rl.execute-api.ap-southeast-1.amazonaws.com/default/dynamodb_update?session_key="+db_session_key+"&db_input=NULL&db_type=4"
         response = requests.get(url)
         form = DecForm()
     context= {'form':form ,'converted':converted}
     return render(request,'accounts/d2b.html',context)
+
+
+def askintent(request,pk):
+    print(pk)
+    db_session_key= request.session.session_key
+    #Shashank AWS url
+    #url = 'https://bzchkm42h7.execute-api.ap-southeast-1.amazonaws.com/default/intent_conclusion_statement?intent='+pk
+    #Christian AWS url
+    url = 'https://l83nginvgb.execute-api.eu-west-2.amazonaws.com/prod/getresponse?session_key='+db_session_key+'&intent='+pk
+    response = requests.get(url)
+    print(response)
+    return HttpResponse(response.json()['response'])
